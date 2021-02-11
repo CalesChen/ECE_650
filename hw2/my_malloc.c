9af593025269ae9f11bfc3f3a81d705afdd589d4
CalesChen <1035134278@qq.com>
2021-02-12 00:10:31 +0800

    hw2 Code Done

---
+++ hw2/my_malloc.c
@@ -0,0 +1,198 @@
+#include "my_malloc.h"
+
+void add_to_List(node_t *ptr, node_t ** head_p){
+  if(ptr == NULL) return;
+  if(*head_p == NULL){
+    *head_p = ptr;
+    free_space_segment_size += ptr->size + sizeof(node_t);
+    return;
+  }
+  
+  ptr->next = *head_p;
+  *head_p = ptr;
+  free_space_segment_size += ptr->size + sizeof(node_t);
+
+  node_t *p = *head_p;
+  size_t b_ptr = ptr->size + sizeof(node_t);
+  size_t n_ptr = (size_t)ptr;
+  //mem address of the next;
+  size_t next = n_ptr + b_ptr;
+  // Iterate to find the suitable location in a sorted list
+  while(p != NULL && p->next != NULL){
+    //size_t bytes = p->size + sizeof(node_t);
+    void * p_temp = p->next;
+    if(p_temp == NULL) break;
+    size_t temp = (size_t) p_temp;
+    // If I find the next node, then break
+    if(temp >= next){
+      break;
+    }
+    p = p_temp;
+  }
+
+  // if the ptr is the highest address
+  if(p->next == NULL){
+    *head_p = (*head_p)->next;
+    ptr->next = p->next;
+    p->next = ptr;
+  }
+  else if(p == *head_p){
+    node_t * n_ptr_next = (node_t*)((char*)ptr + ptr->size + sizeof(node_t));
+    if(n_ptr_next == ptr->next){
+      ptr->size += (sizeof(node_t) + ptr->next->size);
+      ptr->next = ptr->next->next;
+    }
+    return;
+  }
+  else{
+    size_t bytes = p->size + sizeof(node_t);
+    size_t temp = (size_t)p;
+    // Check if the two are adjacent the former one and the ptr
+    if(temp + bytes == (size_t)ptr){
+      p->size += (b_ptr);
+      *head_p = (*head_p)->next;
+      node_t * p_next = p->next;
+      node_t* n_p_next = (node_t*)((char*)p + p->size + sizeof(node_t));
+      if(n_p_next == p->next){
+        // Modify the size and the next pointer
+        p->size += (sizeof(node_t) + p_next->size);
+        p->next = p_next->next;
+      }
+    }
+    else{
+      *head_p = (*head_p)->next;
+      ptr->next = p->next;
+      p->next = ptr;
+      // check if we need to merge the next one
+      node_t * n_ptr_next = (node_t*)((char*)ptr + ptr->size + sizeof(node_t));
+      if(n_ptr_next == ptr->next){
+        //modify the size and the next pointer
+        ptr->size += (sizeof(node_t) + ptr->next->size);
+        ptr->next = ptr->next->next;
+      }
+    }
+  }
+}
+
+void bf_free(void *ptr, node_t ** head_p){
+  if(ptr == NULL) return;
+  node_t * node = ptr - sizeof(node_t);
+  add_to_List(node, head_p);
+}
+
+
+node_t* split(node_t * ptr, size_t size){
+  node_t * res = (node_t *)((char *)ptr + ptr->size - size );
+  res->size = size;
+  //  res->free = 0;
+  res->next = NULL;
+  ptr->size = ptr->size - size - sizeof(node_t);
+  // In malloc func we will return res + 1
+  return res;
+}
+
+node_t* find_bf(size_t size, node_t ** head_p){
+  node_t * curr = *head_p;
+  node_t * res = NULL;
+  //unsigned int min = 65535;
+  
+  while(curr != NULL){
+    if(curr->size >size + sizeof(node_t)){
+      if(res == NULL || curr->size < res->size){
+	res = curr;
+      }
+    }
+    else if(curr->size >= size){
+      return curr;
+    }
+    curr = curr->next;
+  }
+  return res;
+}
+
+void * sbrkChoice(size_t actual_size, int sbrk_lock){
+  node_t * res = NULL;
+  if(sbrk_lock == 0){
+      res = sbrk(actual_size);
+    }
+    else if(sbrk_lock == 1){
+      pthread_mutex_lock(&lock);
+      res = sbrk(actual_size);
+      pthread_mutex_unlock(&lock);
+    }
+  return res;
+}
+
+void * bf_malloc(size_t size, node_t ** head_p, int sbrk_lock){
+  node_t *res = NULL;
+  if(size == 0) return res;
+  size_t actual_size = size + sizeof(node_t);
+  node_t *bf = find_bf(size, head_p);
+  if(*head_p == NULL || bf == NULL){
+
+    res = sbrkChoice(actual_size, sbrk_lock);
+    res->size = size;
+    //res->free = 0;
+    res->next = NULL;
+    if(res != NULL){
+      data_segment_size += actual_size;
+    }
+    return res = NULL ? NULL : res + 1;
+  }
+  else{
+    if(bf->size > actual_size){
+      res = split(bf, size);
+      free_space_segment_size -= actual_size;
+    }
+    else if(bf->size <= actual_size){
+      res = bf;
+      free_space_segment_size -= (bf->size + sizeof(node_t));
+      // Remove the node from the List
+      node_t * p = head;
+      if(p == res){
+        *head_p = (*head_p)->next;
+        //res->free = 0;
+        res->next = NULL;
+        p = NULL; // Then we can skip over the next iteration
+      }
+      while(p != NULL){
+        if(p->next == res){
+          p->next = res->next;
+          //res->free = 0;
+          res->next = NULL;
+          break;
+        }
+        p = p->next;
+      }
+    }
+  }
+  //if(res != NULL){
+  //  free_space_segment_size -= actual_size;
+  //}
+  return res + 1;
+}
+void * ts_malloc_lock(size_t size){
+  pthread_mutex_lock(&lock);
+  int sbrk_lock = 0;
+  void * p = bf_malloc(size, &head, sbrk_lock);
+  pthread_mutex_unlock(&lock);
+  return p;
+}
+void ts_free_lock(void * ptr){
+  pthread_mutex_lock(&lock);
+  bf_free(ptr, &head);
+  pthread_mutex_unlock(&lock);
+}
+
+void * ts_malloc_unlock(size_t size){
+  pthread_mutex_lock(&lock);
+  int sbrk_lock = 1;
+  void * p = bf_malloc(size, &head_tls, sbrk_lock);
+  pthread_mutex_unlock(&lock);
+  return p;
+}
+void ts_free_unlock(void * ptr){
+  pthread_mutex_lock(&lock);
+  bf_free(ptr, &head_tls);
+  pthread_mutex_unlock(&lock);
+}
